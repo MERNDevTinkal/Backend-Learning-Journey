8f905b68841b21b68bff2ef6d6d1c735dbe7b0de
Tinkal Kumar <tinkalkumar67693@gmail.com>
2025-04-07 12:46:30 +0530

    creating first task creater project

--- KeyConcepts/index.js
+++ KeyConcepts/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require('node:fs');
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// GET route
 app.get("/", function (req, res) {
-    res.render("index");
+    fs.readdir("./files", function (err, files) {
+        res.render("index", { files: files });
+    });
 });
 
+// POST route 
+app.post("/create", function (req, res) {
+    fs.writeFile(`./files/${req.body.Title.split(" ").join("")}.txt`, req.body.details, function (err) {
+        res.redirect('/');
+    });
+});
 
-app.listen(3000);   
+//  Server listen bhi bahar hi hona chahiye
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
